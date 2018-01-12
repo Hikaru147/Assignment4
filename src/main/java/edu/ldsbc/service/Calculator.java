cc7cc60fd122cbd3ea7e622a17d82cb69d6cf376
Hikaru147 <shawn.woodford@gmail.com>
2018-01-11 19:18:31 -0700

    Hi - R

---
+++ src/main/java/edu/ldsbc/service/Calculator.java
@@ -0,0 +1,26 @@
+package edu.ldsbc.service;
+
+/**
+ * @author Robert Woodford
+ * January 11, 2018
+ * This is a calculator.
+ */
+
+public class Calculator {
+
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+}
